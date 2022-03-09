87325aa52ddb55eb45e6d11ed64253b089006c9c
Matthew Kauff <matthew.r.kauff@gmail.com>
2022-03-09 11:58:26 -0500

    add comments

--- main.js
+++ main.js
@@ -1,16 +1,4 @@
 "use strict"
-/*
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-}
-*/
 
 // from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
@@ -29,25 +17,44 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+/* TODO: Create Filtered Array function */
 
-// var roastSelection = document.querySelector('#roast-selection');
-
-// duplicate line 71-74
-// submitButton.addEventListener('click', updateCoffees);
-
+// Creates var list by grabbing the li elements printed from addList
 let list = document.getElementById('coffeeList');
 
-let addList = (array, element) => {
-    element.innerHTML = "";
-    array.forEach(item => {
-        let li = document.createElement('li');
-        li.textContent = item.name;
-        element.appendChild(li);
+// Goes through to create a li for each coffee name/ roast type (printing the coffees array)
+let addList = (array, element) => {  // create function with 2 variables
+    element.innerHTML = ""; //clear element - otherwise it would add coffee
+    array.forEach(item => { //Callback function for each element in the array
+        let li = document.createElement('li'); //Creates an individual li for each coffees.name
+        li.textContent = item.name + ' ' + item.roast; //Adds the name and roast to each li
+        element.appendChild(li); //Adds each li to the parent list
     });
 }
 
-document.getElementById("userInput").addEventListener("input", e => {
+// Listens to the keyup from user input (as user types, the array is filtered
+document.getElementById("userInput").addEventListener("input", e => { //Listening to input textbox with id userInput
+    //.filter takes coffees array and ...
     let filteredArray = coffees.filter(c => c.name.toUpperCase().indexOf(e.target.value.toUpperCase()) > -1);
+    //Takes list with coffee/roast names and applies filter capabilities and prints the list
     addList(filteredArray, list);
 });
+//Calls array and prints on html page
 addList(coffees, list);
+
+
+let  roastSelection = document.querySelector('#roast-selection');
+let submitButton = document.querySelector('#submit');
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    let selectedRoast = roastSelection.value;
+    let filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    form.innerHTML = addList(coffees, list);
+}
+submitButton.addEventListener('click', updateCoffees);
